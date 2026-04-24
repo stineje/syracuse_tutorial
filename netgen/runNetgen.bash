ef1ebe234987e1f1aff2b67bdb07b2580a689db2
James Stine <james.stine@okstate.edu>
2026-04-24 08:59:07 -0500

    update netgen

--- netgen/runNetgen.bash
+++ netgen/runNetgen.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-netgen -batch lvs "../magic/$1.spice $1" "../xschem/$1.spice $1" $PDK_ROOT/sky130A/libs.tech/netgen/sky130A_setup.tcl $1.out
+netgen -batch lvs "../lib/${DESIGN_TRACK}/ngspice/$1.spice $1" "../lib/xschem/${DESIGN_TRACK}/spice/$1.spice $1" ${PDK_ROOT}/gf180mcuC/libs.tech/netgen/gf180mcuC_setup.tcl $1.out
